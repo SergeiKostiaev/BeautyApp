393e5ea2544a9b35f2de6a81bc56f0ef1adf37f0
Sergei <skprod.info@gmail.com>
2024-07-17 17:35:21 +0400

    server update3.0

--- server/server.js
+++ server/server.js
@@ -26,13 +26,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Подключение к MongoDB
-mongoose.connect('mongodb://localhost:27017/beauty-booking', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://127.0.0.1:27017/beauty-booking', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log('MongoDB connected');
     })
     .catch((err) => {
         console.error('MongoDB connection error:', err);
     });
+
 
 // Middleware для обработки статических файлов
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
