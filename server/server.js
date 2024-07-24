c42a25cd98b756504722da44378110a19955ca09
Sergei Kostiaev <88935925+SergeiKostiaev@users.noreply.github.com>
2024-07-24 23:29:02 +0400

    Update server.js 2.0

--- server/server.js
+++ server/server.js
@@ -174,11 +174,11 @@ app.post('/api/time-slots', async (req, res) => {
 });
 
 // Сервирование статических файлов из папки build
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 // Обработка всех остальных запросов
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
 app.listen(PORT, () => {
