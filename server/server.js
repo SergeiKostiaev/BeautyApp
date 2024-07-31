b94b9108b3f3faf6867e4497499892260ce18cf1
Sergei <skprod.info@gmail.com>
2024-07-31 10:19:46 +0400

    update server

--- server/server.js
+++ server/server.js
@@ -24,22 +24,22 @@ const sendToTelegram = require('./Telegram');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const corsOptions = {
-    origin: 'https://devprimeclients.ru/', // ваш фронтенд URL из переменной окружения
-    optionsSuccessStatus: 200,
-};
-// CORS настройки
 // const corsOptions = {
-//     origin: (origin, callback) => {
-//         const allowedOrigins = ['http://localhost:3000', 'https://devprimeclients.ru/'];
-//         if (!origin || allowedOrigins.includes(origin)) {
-//             callback(null, true);
-//         } else {
-//             callback(new Error('Not allowed by CORS'));
-//         }
-//     },
+//     origin: 'https://devprimeclients.ru/', // ваш фронтенд URL из переменной окружения
 //     optionsSuccessStatus: 200,
 // };
+//CORS настройки
+const corsOptions = {
+    origin: (origin, callback) => {
+        const allowedOrigins = ['http://31.172.75.47:3000', 'https://devprimeclients.ru/'];
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    optionsSuccessStatus: 200,
+};
 
 app.use(cors(corsOptions));
 app.use(express.json());
